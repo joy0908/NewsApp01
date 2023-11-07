5b9ef97f81a896153a0bdf375c0175deecbe32de
Student <Student@205-45>
2023-11-07 16:07:46 +0800

    加入專案檔案。

---
+++ NewsApp/Pages/NewsDetailPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace NewsAPP.Pages;
+
+public partial class NewsDetailPage : ContentPage
+{
+	public NewsDetailPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ NewsApp/Pages/NewsListPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace NewsAPP.Pages;
+
+public partial class NewsListPage : ContentPage
+{
+	public NewsListPage(string name)
+	{
+		InitializeComponent();
+	}
+}
